d9958ef72db0e2a98ec844250da31f854f475c44
sannma31 <kasaisyouun2004@gmail.com>
2024-04-13 23:08:00 +0900

    Initial Commit

---
+++ Mymusic/MymusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MymusicApp.swift
+//  Mymusic
+//
+//  Created by 笠井翔雲 on 2024/04/13.
+//
+
+import SwiftUI
+
+@main
+struct MymusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
