e11e3bc04638147510fc428db3f2fcd4b0dbba79
sannma31 <kasaisyouun2004@gmail.com>
2024-04-14 21:22:17 +0900

    add:写真の挿入

--- Mymusic/ContentView.swift
+++ Mymusic/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image(.blueback)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            HStack{
+                Button(action: {
+                    
+                }, label: {
+                    Image(.cymbal)
+                })
+                Button(action: {
+                    
+                }, label: {
+                    Image(.guitar)
+                })
+            }
         }
-        .padding()
     }
 }
 
